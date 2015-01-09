011024f65c9fd51e2e7418dc692e44d9ef86656c
Peter Spier <spierp@gmail.com>
2015-01-09 11:51:33 -0800

    postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  ActionMailer::Base.smtp_settings = {
+  :port           => '25', # or 2525
+  :address        => ENV['POSTMARK_SMTP_SERVER'],
+  :user_name      => ENV['POSTMARK_API_KEY'],
+  :password       => ENV['POSTMARK_API_KEY'],
+  :domain         => 'hdseattle.herokuapp.com',
+  :authentication => :cram_md5, # or :plain for plain-text authentication
+  :enable_starttls_auto => true, # or false for unencrypted connection
+}
+ActionMailer::Base.delivery_method = :smtp
 
 end
