5b9af7ed0d9314f2df25921d419f6534849a9232
Peter Spier <spierp@gmail.com>
2015-01-15 10:39:48 -0800

    edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,28 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    if current_user.admin?
+      @user = User.find(params[:id])
+    else
+      redirect_to root_path
+    end
+  end
+
   def destroy
     @user = User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_path
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params.require(:user).permit(:first_name, :last_name, :image, :admin))
+      flash[:success] = "User Updated"
+      redirect_to "/users"
+    else
+      render 'edit'
+    end
   end
 
 end
